0988d07f2b432f6926cd221b59503fa4c0174d92
Nathaniel Ho <nxho93@gmail.com>
2024-12-21 13:55:25 -0500

    add kill script

---
+++ computer-use-demo/kill.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+container_id=$(docker ps --filter "ancestor=computer-use-demo:local" --format "{{.ID}}")
+docker kill "${container_id}" 
+
+web_app_pid=$(pgrep "npm run dev")
+kill "${web_app_pid}"
+
